77feabdb41d130480aaa1b0b33c01052816415d1
Eduardo Elias Alves Pereira <eduardo.eliasap@gmail.com>
2020-07-20 15:49:54 -0300

    Appointments routes

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,23 @@
+import React from 'react';
 /* The createSwitchNavigator storage on a historic navigation */
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import {
+  createAppContainer,
+  createSwitchNavigator
+} from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 
 import Dashboard from './pages/Dashboard';
 import Profile from './pages/Profile';
+
+import SelectProvider from './pages/New/SelectProvider';
+import SelectDateTime from './pages/New/SelectDateTime';
+import Confirm from './pages/New/Confirm';
 
 export default (signedIn = false) => createAppContainer(createSwitchNavigator({
   Sign: createSwitchNavigator({
@@ -16,6 +27,28 @@ export default (signedIn = false) => createAppContainer(createSwitchNavigator({
   App: createBottomTabNavigator(
     {
       Dashboard,
+      New: {
+        screen: createStackNavigator({
+          SelectProvider,
+          SelectDateTime,
+          Confirm,
+        }, {
+          defaultNavigationOptions: {
+            headerTransparent: true,
+            headerTintColor: '#FFF',
+            headerLeftContainerStyle: {
+              marginLeft: 20,
+            }, // It will only be applied to New routes
+          },
+        }),
+        navigationOptions: { // Creates a navigation stack
+          tabBarVisible: false, // It will clear the status bar when entering these routes. It is necessary to force the user to not navigate routes outside the New routes
+          tabBarLabel: 'Agendar',
+          tabBarIcon: (
+            <Icon name='add-circle-outline' size={20} color="rgba(255, 255, 255, 0.1)" />
+          )
+        }
+      },
       Profile,
     },
     {
